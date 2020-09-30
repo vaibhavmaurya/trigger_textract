e3c44c9f80da2d9873ad9ce31bce71e6202ebb04
EC2 Default User <ec2-user@ip-172-31-30-15.ap-southeast-1.compute.internal>
2020-09-30 08:07:37 +0000

    textract basic code

--- index.py
+++ index.py
@@ -1,8 +1,27 @@
 import json
 import datetime
+import boto3
 
 
 def handler(event, context):
+    bucket="bucket"
+    document="document"
+    client = boto3.client('textract')
+
+
+
+    #process using S3 object
+    response = client.detect_document_text(
+        Document={'S3Object': {'Bucket': bucket, 'Name': document}})
+
+    #Get the text blocks
+    blocks=response['Blocks']
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(blocks)
+    }
+    
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
